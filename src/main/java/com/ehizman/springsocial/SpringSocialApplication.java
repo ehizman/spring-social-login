4b16572a9e3b82f493d456360a274061e4ee6d1c
Ehis <edemaehiz@gmail.com>
2021-11-22 14:23:13 +0100

    created configuration for app properties configuration for the auth providers defined the user model and the user repository interface

---
+++ src/main/java/com/ehizman/springsocial/SpringSocialApplication.java
@@ -0,0 +1,17 @@
+package com.ehizman.springsocial;
+
+import com.ehizman.springsocial.config.AppProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
+public class SpringSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ehizman/springsocial/model/AuthProvider.java
@@ -0,0 +1,8 @@
+package com.ehizman.springsocial.model;
+
+public enum AuthProvider {
+    LOCAL,
+    FACEBOOK,
+    GOOGLE,
+    GITHUB
+}
